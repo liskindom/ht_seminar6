8c277b492e8ac2d52ec5c8894b1acc1644f06bb3
lizaveta_mikulich <lizavetamikulich@2.pl>
2024-03-12 20:55:24 +0100

    Решение задачи 3

---
+++ ht_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+
+void PrintNumbers(int[] numbers, int i)
+{
+    if (i < 0)
+    {
+        return;
+    }
+    Console.Write(numbers[i] + " ");
+
+    PrintNumbers(numbers, i-1);
+
+}
+int[] numbers = {1, 3, 5, 7, 9};
+
+PrintNumbers(numbers, numbers.Length - 1);
+
